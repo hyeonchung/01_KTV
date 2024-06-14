a38109569e1fa8a0340b97ec3fb822fca9f0afed
Hyeon <hyeonjoon.ch@gmail.com>
2024-06-14 15:41:58 +0900

    최근 관람목록 Container, ItemCell 제작중

---
+++ KTV/Home/Cell/HomeRecentWatchContainerCell.swift
@@ -0,0 +1,20 @@
+//
+//  HomeRecentWatchContainerCell.swift
+//  KTV
+//
+//  Created by 정현준 on 6/14/24.
+//
+
+import UIKit
+
+class HomeRecentWatchContainerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
---
+++ KTV/Home/Cell/HomeRecentWatchItemCellCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeRecentWatchItemCellCollectionViewCell.swift
+//  KTV
+//
+//  Created by 정현준 on 6/14/24.
+//
+
+import UIKit
+
+class HomeRecentWatchItemCellCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
